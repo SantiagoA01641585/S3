23e8a9704539e31d808dc5fb9f513ee8eee42a9a
SantiagoA01641585 <a01641585@itesm.mx>
2022-08-11 08:36:45 -0500

    Direct

---
+++ PRODASFA/Homework_1/Direct/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout << "Direct sum of numbres to n program:\n" << endl;
+    cout << ">> ";
+
+    cin >> n;
+
+    cout << "\n" << endl;
+
+    if(n>=1) cout << (n*(n+1))/2 << endl;
+    else cout << 0 << endl;
+}
