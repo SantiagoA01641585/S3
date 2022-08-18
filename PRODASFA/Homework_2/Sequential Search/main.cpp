341c7ba511b21ea7d809eb21e27ca812c9e51f0d
SantiagoA01641585 <a01641585@itesm.mx>
2022-08-18 15:20:41 -0500

    1.2.1.5

---
+++ PRODASFA/Homework_2/Sequential Search/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int sequentialSearch(int search, vector <int> input){ 
+
+    for (int i=0; i<input.size(); i++){ // Iterates trough the vector
+        if (input[i] == search) return i; // If the number chosen is found, the functions returns the ID
+    }
+
+    return -1; // Returns -1 otherwise
+}
+
+int main(){
+
+    vector <int> a = {1, 4, 2, 9, 7, 8, 10}; // Test vector
+
+    int ans = sequentialSearch(2, a); // Result of the search
+
+    if (ans > -1) { // If it is found say where in the console
+        cout << "Number found at " << ans << "!";
+    }
+    else { // If not, say so
+        cout << "Number not found.";
+    }
+
+}
