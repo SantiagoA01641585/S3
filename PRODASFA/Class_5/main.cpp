1b6d5e9956fe28359886d7f2d48c937984d43734
SantiagoA01641585 <a01641585@itesm.mx>
2022-09-26 22:00:21 -0500

    1.4.0.7

--- PRODASFA/Class_5/main.cpp
+++ PRODASFA/Class_5/main.cpp
@@ -5,14 +5,15 @@ using namespace std;
 struct Node{
     int data;
     Node *left, *right;
-
+    
+    Node (): data(0), left(NULL), right(NULL){}
     Node (int d): data(d), left(NULL), right(NULL){}
 };
 
 class BST{
     private:
         Node *root;
-        void Insert(int &, Node *&);
+        void insert(int &, Node *&);
         void InOrder(Node *);
         void PreOrder(Node *);
         void PostOrder(Node *);
@@ -27,7 +28,7 @@ class BST{
         }
 
         // Functions
-        void insert(int &value){Insert(value, root);}
+        void insert(int &value){insert(value, root);}
         void InOrder(){InOrder(root);}
         void PreOrder(){PreOrder(root);}
         void PostOrder(){PostOrder(root);}
@@ -37,16 +38,16 @@ class BST{
         void DeleteBST(Node *&);
 };
 
-void BST::Insert(int &value, Node *&root){
+void BST::insert(int &value, Node *&root){
     if(root==NULL){
         root = new Node(value);
     }
     else{
         if(value < root->data){
-            Insert(value, root->left);
+            insert(value, root->left);
         }
         else if(value > root->data){
-            Insert(value, root->right);
+            insert(value, root->right);
         }
         else{
             cout << "Repeated element.\n";
@@ -59,21 +60,54 @@ void BST::print(Node *root){
         return;
     }
     else{
-        cout << root->data;
+        cout << root->data << endl;
 
-        if(root->left) print(root->left);
-        if(root->right) print(root->right);
+        if(root->left) cout << root->left->data << " ";
+        if(root->right) cout << root->right->data;
+
+        cout << endl;
+
+        if(root->left->left) print(root->left->left);
+        cout << " ";
+        if(root->left->right) print(root->left->right);
+        cout << " ";
+        if(root->right->left) print(root->right->left);
+        cout << " ";
+        if(root->right->right) print(root->right->right);
     }
+}
+
+void BST::DeleteBST(Node *& root){
+    if(root){
+        DeleteBST(root->left);
+        DeleteBST(root->right);
+
+        delete root;
+        return;
+    }
+
+    return;
 }
 
 int main(){
     BST tree;
 
     int val = 5;
-
+    tree.insert(val);
+    val = 6;
+    tree.insert(val);
+    val = 3;
+    tree.insert(val);
+    val = 9;
+    tree.insert(val);
+    val = 4;
+    tree.insert(val);
+    val = 1;
+    tree.insert(val);
+    val = 11;
     tree.insert(val);
 
-    //tree.print();
+    tree.print();
 
 
 }
