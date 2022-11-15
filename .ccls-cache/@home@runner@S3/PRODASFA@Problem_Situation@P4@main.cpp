26d0bfe435977214f830a873c012b82b10884636
Santiago Vera <88670016+SantiagoA01641585@users.noreply.github.com>
2022-11-15 03:57:13 +0000

    1.4.6.7

--- .ccls-cache/@home@runner@S3/PRODASFA@Problem_Situation@P4@main.cpp
+++ .ccls-cache/@home@runner@S3/PRODASFA@Problem_Situation@P4@main.cpp
@@ -44,8 +44,8 @@ class Graph{
     Node *arr_nodes;
     bool *visited;
 
-    pair <int, int> max_in_degree;
-    pair <int, int> max_out_degree;
+    pair <int, int> max_in_degree; // First element is ip, second is degree
+    pair <int, int> max_out_degree; // First element is ip, second is degree
   
   public:
     // Constructor
@@ -62,6 +62,9 @@ class Graph{
       visited = new bool[V];
 
       arr_nodes = new Node[V];
+
+      max_in_degree = {-1, -1};
+      max_out_degree = {-1, -1};
     }
 
   // Destructor
@@ -75,6 +78,9 @@ class Graph{
   void ResetVisited();
   void setNode(string line);
   void setNodes(int[]);
+
+  pair <int, int> getMaxInDegree();
+  pair <int, int> getMaxOutDegree();
 };
 
 // Add Edge Function - Complexity O(1)
@@ -138,30 +144,52 @@ vector<int> getIP(string line) // Separates the IP from the strings given by the
 
 void Graph::setNode(string line){
   vector<int> ip =  getIP(line);
-  int first = 0;
-  int sec = 1;
+
+  for (int i=0; i<ip.size() - 1; i++){
+    adjMatrix[ip[i]][ip[i+1]] = true;
+
+    arr_nodes[ip[i]].setOutDegree(arr_nodes[ip[i]].getOutDegree() + 1);
+    arr_nodes[ip[i + 1]].setInDegree(arr_nodes[ip[i + 1]].getInDegree() + 1);
+
+    if (this->arr_nodes[ip[i]].getOutDegree() > this->max_out_degree.second)
+      this->max_out_degree = {ip[i], this->arr_nodes[ip[i]].getOutDegree()};
+
+    if (this->arr_nodes[ip[i + 1]].getInDegree() > this->max_in_degree.second)
+      this->max_in_degree = {ip[i + 1], this->arr_nodes[ip[i + 1]].getInDegree()};
+  }
+}
+
+pair <int, int> Graph::getMaxInDegree(){
+  return this->max_in_degree;
+}
+
+pair <int, int> Graph::getMaxOutDegree(){
+  return this->max_out_degree;
 }
 
 int main(){
   ifstream infile;
   ofstream outfile;
 
-  cout << "Hola2\n";
-
   std::vector<std::string> lines;
   string line;
+  
+  Graph graph(1000);
 
   // Open the input file
   infile.open("bitacora.txt");
 
   // If the file is open
   while (getline(infile, line)){ 
-    
+    graph.setNode(line);
   }
 
   // Close the input file
   infile.close();
-  
-  Graph graph(1000);
+
+  cout << "Max InDegree: " << graph.getMaxInDegree().first << " - " << 
+    graph.getMaxInDegree().second << endl;
+  cout << "Max OutDegree: " << graph.getMaxOutDegree().first << " - " <<           
+    graph.getMaxOutDegree().second << endl;
   
 }
